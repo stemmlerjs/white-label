8822290ac42751e4b3f9439c1c61c649208334a9
Khalil Stemmler <metroidman12@gmail.com>
2019-06-24 15:12:54 -0400

    Update tsconfig and start a couple of aggregate roots + entities.

---
+++ src/trading/domain/Trader.ts
@@ -0,0 +1,20 @@
+
+import { AggregateRoot } from "../../core/domain/AggregateRoot";
+import { UniqueEntityID } from "../../core/domain/UniqueEntityID";
+import { Result } from "../../core/Result";
+import { UserId } from "../../users/domain/UserId";
+
+interface TraderProps {
+  userId: UserId;
+  reputation: number;
+}
+
+export class Trader extends AggregateRoot<TraderProps> {
+  private constructor (props: TraderProps, id?: UniqueEntityID) {
+    super(props, id);
+  }
+
+  public create (props: TraderProps, id?: UniqueEntityID): Result<Trader> {
+    return null;
+  }
+}
---
+++ src/users/domain/User.ts
@@ -0,0 +1,17 @@
+import { AggregateRoot } from "../../core/domain/AggregateRoot";
+import { UniqueEntityID } from "../../core/domain/UniqueEntityID";
+import { Result } from "../../core/Result";
+
+interface UserProps {
+
+}
+
+export class User extends AggregateRoot<UserProps> {
+  private constructor (props: UserProps, id?: UniqueEntityID) {
+    super(props, id);
+  }
+
+  public static create (props: UserProps, id?: UniqueEntityID): Result<User> {
+    return null;
+  }
+}

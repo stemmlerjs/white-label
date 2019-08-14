b004383baff04c04a9e6c5ca1bb964ffe0fc2fb0
Khalil Stemmler <metroidman12@gmail.com>
2019-08-14 12:37:20 -0400

    Finish functional error handling.

--- src/functional-error-handling.ts
+++ src/functional-error-handling.ts
@@ -2,6 +2,7 @@
 import { Either, Result, left, right } from "./core/Result";
 import { UseCase } from "./core/domain/UseCase";
 import { BaseController } from "./core/infra/BaseController";
+import { ValueObject } from "./core/domain/ValueObject";
 
 /**
  * Resources
@@ -20,22 +21,32 @@ interface DomainError {
   error?: any;
 }
 
-// This is how to many an 'any', number of parameters to an interface
+/**
+ * @desc General application errors (few of these as possible)
+ * @http 500
+ */
 
-type DomainErrorFactoryFunction = (...args: any[]) => DomainError;
+export namespace AppError {
+  export class UnexpectedError extends Result<DomainError> {
+    public constructor (err: any) {
+      super(false, {
+        message: `An unexpected error occurred.`,
+        error: err
+      })
+    }
 
-// This is how to specify the types of the keys in an object
+    public static create (err: any): UnexpectedError {
+      return new UnexpectedError(err);
+    }
+  }
+}
 
-type DomainErrorMap = { 
-  [x: string]: DomainError | DomainErrorFactoryFunction;
-};
-
-namespace CreateUserError {
+export namespace CreateUserError {
 
   export class UsernameTakenError extends Result<DomainError> {    
-    private constructor (username: string) {
+    public constructor (username: string) {
       super(false, {
-        message: `The username ${username} was already taken`
+        message: `The username "${username}" has already been taken.`
       })
     }
 
@@ -45,56 +56,171 @@ namespace CreateUserError {
   }
 
   export class EmailInvalidError extends Result<DomainError> {    
-    private constructor (email: string, err?: any) {
+    public constructor (email: string) {
       super(false, {
-        message: `The email ${email} was invalid`
+        message: `The email "${email}" is invalid.`
       })
     }
 
-    public static create (email: string, err?: any): EmailInvalidError {
-      return new EmailInvalidError(email, err);
+    public static create (email: string): EmailInvalidError {
+      return new EmailInvalidError(email);
     }
   }
 
+  export class AccountAlreadyExistsError extends Result<DomainError> {    
+    public constructor () {
+      super(false, {
+        message: `The account associated with this email already exists.`
+      })
+    }
+
+    public static create (): AccountAlreadyExistsError {
+      return new AccountAlreadyExistsError();
+    }
+  }
+
+  export class InsecurePasswordError extends Result<DomainError> {    
+    public constructor () {
+      super(false, {
+        message: `The password provided wasn't up to security standards.`
+      })
+    }
+
+    public static create (): InsecurePasswordError {
+      return new InsecurePasswordError();
+    }
+  }
 }
 
-interface CreateUserUseCaseDTO { username: string, email: string }
+interface Request {
+  username: string;
+  email: string;
+  password: string;
+}
 
-type CreateUserUseCaseResponse = Either<
-  CreateUserError.UsernameTakenError | 
-  CreateUserError.EmailInvalidError
-  , 
-  Result<void>
->
+interface EmailProps {
+  email: string;
+}
 
-class CreateUserUseCase implements UseCase<CreateUserUseCaseDTO, CreateUserUseCaseResponse> {
-  
-  private isEmailInvalid (email: string): boolean {
-    return email.indexOf('.com') === -1;
+export class Email extends ValueObject<EmailProps> {
+
+  get value (): string {
+    return this.props.email
   }
 
-  execute (request: CreateUserUseCaseDTO): CreateUserUseCaseResponse {
+  private constructor (props: EmailProps) {
+    super(props);
+  }
 
-    const { username, email } = request;
+  private static isEmailValid (email: string): boolean {
+    // Naive validation
+    if (email.indexOf('.com') === -1) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 
-    if (username === 'stemmlerjs') {
-      return left(CreateUserError.UsernameTakenError.create(username))
+  public static create (props: EmailProps): Either<CreateUserError.EmailInvalidError, Result<Email>> {
+    if (this.isEmailValid(props.email)) {
+      return right(Result.ok<Email>(new Email(props)));
+    } else {
+      return left(CreateUserError.EmailInvalidError.create(props.email));
+    }
+  }
+}
+
+interface PasswordProps {
+  password: string;
+}
+
+export class Password extends ValueObject<PasswordProps> {
+
+  get value (): string {
+    return this.props.password
+  }
+
+  private constructor (props: PasswordProps) {
+    super(props);
+  }
+
+  private static isPasswordValid (password: string): boolean {
+    // Naive validation
+    if (password.length < 6) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  public static create (props: PasswordProps): Either<CreateUserError.InsecurePasswordError, Result<Password>> {
+    if (this.isPasswordValid(props.password)) {
+      return right(Result.ok<Password>(new Password(props)));
+    } else {
+      return left(CreateUserError.InsecurePasswordError.create());
+    }
+  }
+}
+
+interface User {}
+
+interface IUserRepo {
+  getUserByUsername(username: string): Promise<User>;
+  getUserByEmail(email: string): Promise<User>;
+}
+
+type Response = Either<
+  CreateUserError.UsernameTakenError | 
+  CreateUserError.EmailInvalidError | 
+  CreateUserError.AccountAlreadyExistsError |
+  CreateUserError.InsecurePasswordError
+  , 
+  Result<any> // OK 
+>
+
+class CreateUserUseCase implements UseCase<Request, Promise<Response>> {
+  private userRepo: IUserRepo;
+
+  constructor (userRepo: IUserRepo) {
+    this.userRepo = userRepo;
+  }
+
+  public async execute (request: Request): Promise<Response> {
+
+    const { username, email, password } = request;
+
+    const [userByUsername, userByEmail] = await Promise.all([
+      this.userRepo.getUserByUsername(username),
+      this.userRepo.getUserByEmail(email),
+    ])
+
+    const usernameTaken = !!userByUsername === true;
+    const accountCreated = !!userByEmail === true;
+
+    if (usernameTaken) {
+      return left(CreateUserError.UsernameTakenError.call(username));
     }
 
-    if (this.isEmailInvalid(email)) {
-      return left(CreateUserError.EmailInvalidError.create(email))
-    } 
+    if (accountCreated) {
+      return left(CreateUserError.EmailInvalidError.call(email));
+    }
+
+    const emailOrError = Email.create({ email })
+
+    if (emailOrError.isLeft()) {
+      return left(emailOrError.value);
+    }
+
+    const passwordOrError = Password.create({ password });
+
+    if (passwordOrError.isLeft()) {
+      return left(passwordOrError.value);
+    }
 
     return right(Result.ok())
   }
 }
 
-const useCase = new CreateUserUseCase();
-
-const result = useCase.execute({
-  username: 'stemmlerjs',
-  email: 'billbob@gmail.com'
-})
 
 class CreateUserController extends BaseController {
   private useCase: CreateUserUseCase;
@@ -104,11 +230,11 @@ class CreateUserController extends BaseController {
     this.useCase = useCase;
   }
 
-  executeImpl (): Promise<any> {
-    const { username, email } = this.req.body;
+  async executeImpl (): Promise<any> {
+    const { username, email, password } = this.req.body;
 
     try {
-      const result = this.useCase.execute({ username, email });
+      const result = await this.useCase.execute({ username, email, password });
 
       if (result.isLeft()) {
         const error = result.value;
@@ -117,6 +243,10 @@ class CreateUserController extends BaseController {
           case CreateUserError.UsernameTakenError:
             return this.conflict(error.getValue().message)
           case CreateUserError.EmailInvalidError:
+            return this.clientError(error.getValue().message);
+          case CreateUserError.AccountAlreadyExistsError:
+            return this.conflict(error.getValue().message);
+          case CreateUserError.InsecurePasswordError:
             return this.clientError(error.getValue().message);
           default:
             return this.fail(error.getValue().message);
